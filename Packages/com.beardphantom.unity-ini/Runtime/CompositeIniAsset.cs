323320d362df55e1007c88bd5e6e50a548fde2bc
Mika Notarnicola <mika@beardphantom.com>
2023-10-27 02:32:13 -0700

    Add layers to composite INI

--- Packages/com.beardphantom.unity-ini/Runtime/CompositeIniAsset.cs
+++ Packages/com.beardphantom.unity-ini/Runtime/CompositeIniAsset.cs
@@ -1,4 +1,5 @@
 ﻿using IniParser.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -9,10 +10,37 @@ namespace BeardPhantom.UnityINI
     [CreateAssetMenu(menuName = "INI/Composite INI")]
     public class CompositeIniAsset : IniAssetBase
     {
+        #region Types
+
+        [Serializable]
+        public class Layer
+        {
+            #region Properties
+
+            [field: SerializeField]
+            public bool Enabled { get; set; } = true;
+
+            [field: SerializeField]
+            public IniAsset Asset { get; set; }
+
+            #endregion
+
+            #region Methods
+
+            public static bool IsValidAndEnabled(Layer layer)
+            {
+                return layer is { Enabled: true } && layer.Asset != null;
+            }
+
+            #endregion
+        }
+
+        #endregion
+
         #region Properties
 
         [field: SerializeField]
-        public List<IniAsset> IniAssets { get; private set; } = new();
+        public List<Layer> Layers { get; private set; } = new();
 
         [field: SerializeField]
         public override IniSerializedData Data { get; protected set; }
@@ -24,7 +52,13 @@ namespace BeardPhantom.UnityINI
         public static CompositeIniAsset Create(IEnumerable<IniAsset> iniAssets)
         {
             var compositeIniAsset = CreateInstance<CompositeIniAsset>();
-            compositeIniAsset.IniAssets.AddRange(iniAssets);
+            compositeIniAsset.Layers.AddRange(
+                iniAssets.Select(
+                    i => new Layer
+                    {
+                        Asset = i,
+                        Enabled = true
+                    }));
             compositeIniAsset.RegenerateData();
             return compositeIniAsset;
         }
@@ -32,17 +66,17 @@ namespace BeardPhantom.UnityINI
         [ContextMenu("Regenerate INI Data")]
         public void RegenerateData()
         {
-            using var _ = ListPool<IniAsset>.Get(out var valid);
-            valid.AddRange(IniAssets.Where(i => i != null));
+            using var _ = ListPool<Layer>.Get(out var valid);
+            valid.AddRange(Layers.Where(Layer.IsValidAndEnabled));
             if (valid.Count == 0)
             {
                 return;
             }
 
-            var parsedData = new IniData(valid[0].IniParsedData);
+            var parsedData = new IniData(valid[0].Asset.IniParsedData);
             for (var i = 1; i < valid.Count; i++)
             {
-                parsedData.Merge(valid[i].IniParsedData);
+                parsedData.Merge(valid[i].Asset.IniParsedData);
             }
 
             Data = new IniSerializedData(parsedData);
